a72663625b2f315f899bcdb40dadf36965ae3cc3
huoweiguo <huoweiguo123>
2024-03-22 15:01:23 +0800

    modify utils

--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -1,7 +1,12 @@
-'use strict';
+'use strict'
 
-module.exports = utils;
+module.exports = utils
 
 function utils() {
-  return 'Hello from utils';
+  return 'Hello from utils'
+}
+
+function Date() {
+  var d = new Date()
+  return d
 }

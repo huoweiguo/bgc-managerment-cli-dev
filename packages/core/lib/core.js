f5c7bba75b06e4424a60312c2f5d6b22ae6e1915
huoweiguo <huoweiguo123>
2024-03-22 22:29:22 +0800

    add axios package

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
 const utils = require('@bgc-managerment-cli-dev/utils')
+const axios = require('@bgc-managerment-cli-dev/axios')
 
 utils.getMsg()
+axios.get('http://www.baidu.com')
 console.log('hsdhhddh')
